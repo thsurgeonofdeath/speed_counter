748094444294b2cc6d4da5111390b95de87d027f
thsurgeonofdeath <salahek25@gmail.com>
2023-02-20 16:37:10 +0100

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,50 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect, useRef} from "react"
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
-  const STARTING_TIME = 5
-    
-  const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-  const textBoxRef = useRef(null)
-  
-  function handleChange(e) {
-      const {value} = e.target
-      setText(value)
-  }
-  
-  function calculateWordCount(text) {
-      const wordsArr = text.trim().split(" ")
-      return wordsArr.filter(word => word !== "").length
-  }
-  
-  function startGame() {
-      setIsTimeRunning(true)
-      setTimeRemaining(STARTING_TIME)
-      setText("")
-      textBoxRef.current.disabled = false
-      textBoxRef.current.focus()
-  }
-  
-  function endGame() {
-      setIsTimeRunning(false)
-      setWordCount(calculateWordCount(text))
-  }
-  
-  // https://www.google.com/search?q=Disable+button+in+react
-  
-  useEffect(() => {
-      if(isTimeRunning && timeRemaining > 0) {
-          setTimeout(() => {
-              setTimeRemaining(time => time - 1)
-          }, 1000)
-      } else if(timeRemaining === 0) {
-          endGame()
-      }
-  }, [timeRemaining, isTimeRunning])
+
+  const {textBoxRef,
+        handleChange,
+        text,
+        isTimeRunning,
+        timeRemaining,
+        startGame,
+        wordCount} = useWordGame(15)
   
   return (
       <div>

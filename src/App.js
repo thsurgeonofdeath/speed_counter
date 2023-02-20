5098015f739e8eae83d9b0f531f0000ac3641065
thsurgeonofdeath <salahek25@gmail.com>
2023-02-20 15:41:21 +0100

    timer hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 function App() {
-
-  const [text, setText] = useState("")
-  const [wordCount,setWordCount] = useState(0)
+  const STARTING_TIME = 5
     
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+  
   function handleChange(e) {
-    const {value} = e.target
-    setText(value)
+      const {value} = e.target
+      setText(value)
   }
-
+  
   function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    setWordCount(wordsArr.filter(word => word !== "").length)
+      const wordsArr = text.trim().split(" ")
+      return wordsArr.filter(word => word !== "").length
   }
-
+  
+  function startGame() {
+      setIsTimeRunning(true)
+      setTimeRemaining(STARTING_TIME)
+      setText("")
+  }
+  
+  function endGame() {
+      setIsTimeRunning(false)
+      setWordCount(calculateWordCount(text))
+  }
+  
+  // https://www.google.com/search?q=Disable+button+in+react
+  
+  useEffect(() => {
+      if(isTimeRunning && timeRemaining > 0) {
+          setTimeout(() => {
+              setTimeRemaining(time => time - 1)
+          }, 1000)
+      } else if(timeRemaining === 0) {
+          endGame()
+      }
+  }, [timeRemaining, isTimeRunning])
+  
   return (
-    <div className="App">
-      <h1>How fast do you type?</h1>
-      <textarea
-        onChange={handleChange}
-        value={text}
-      />
-      <h4>Time reminaing: ???</h4>
-      <button onClick={() => calculateWordCount(text)}>Start</button>
-      <h1>Word count: {wordCount}</h1>
-    </div>
-  );
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea
+              onChange={handleChange}
+              value={text}
+              disabled={!isTimeRunning}
+          />
+          <h4>Time remaining: {timeRemaining}</h4>
+          <button 
+              onClick={startGame}
+              disabled={isTimeRunning}
+          >
+              Start
+          </button>
+          <h1>Word count: {wordCount}</h1>
+      </div>
+  )
 }
 
 export default App;

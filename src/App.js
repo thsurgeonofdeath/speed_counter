be49dfa4b42b6368670f8a4d2f02b53f64e7e0e7
thsurgeonofdeath <salahek25@gmail.com>
2023-02-20 14:51:47 +0100

    text input hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+
+  const [text, setText] = useState("")
+    
+  function handleChange(e) {
+    const {value} = e.target
+    setText(value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How fast do you type?</h1>
+      <textarea
+        onChange={handleChange}
+        value={text}
+      />
+      <h4>Time reminaing: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }
